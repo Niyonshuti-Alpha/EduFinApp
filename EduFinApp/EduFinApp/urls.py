7cb44a806d02eccf4ac2e1a0723ef6511c146f6d
NIYONSHUTI Alpha <niyonshutialpha7@gmail.com>
2026-03-11 14:25:31 +0200

    Complete EduFinApp - All steps done

--- EduFinApp/EduFinApp/urls.py
+++ EduFinApp/EduFinApp/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import testing_view
+from core.views import testing_view, testing_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('testing', testing_view, name='testing'),
+    path('testing/<int:id>', testing_detail_view, name='testing-detail'),
 ]
--- EduFinApp/core/views.py
+++ EduFinApp/core/views.py
@@ -1,7 +1,17 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from core.models import Testing
+from core.serializers import TestingSerializer
 
 def testing_view(request):
-    # For now, return a simple static JSON response
-    return JsonResponse({'message': 'Hello, world!'})
+    testings = Testing.objects.all()
+    serializer = TestingSerializer(testings, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+def testing_detail_view(request, id):
+    try:
+        testing = Testing.objects.get(id=id)
+        serializer = TestingSerializer(testing)
+        return JsonResponse(serializer.data)
+    except Testing.DoesNotExist:
+        return JsonResponse({'error': 'Record not found'}, status=404)
